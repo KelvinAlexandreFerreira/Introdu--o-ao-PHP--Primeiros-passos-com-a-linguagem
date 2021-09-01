65e6938f7b76451c666189cfb11727efa7675434
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-09-01 07:02:59 -0400

    Senão

--- php-primeiros-passos/decisoes.php
+++ php-primeiros-passos/decisoes.php
@@ -1,12 +1,17 @@
 <?php
 
-$idade = 21;
-$nome = 'Vinicius';
+$idade = 15;
 
 
 echo "Você só pode entrar, se tiver a partir de 18 anos." . PHP_EOL;
 
-if ($idade >= 18 && $nome == 'Vinicius'){
+if ($idade >= 18){
     echo "Você tem $idade anos." . PHP_EOL;
     echo 'Pode entrar.' . PHP_EOL;    
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar." . PHP_EOL;
 }
+
+echo PHP_EOL;
+
+echo "Adeus.";
